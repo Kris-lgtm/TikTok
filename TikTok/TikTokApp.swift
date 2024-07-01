226aad154cab1b08499e5ec89bba57a071bb953c
mbook <mbook@users-MacBook-Pro.local>
2024-07-01 19:43:28 +0300

    Создание проекта

---
+++ TikTok/TikTokApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokApp.swift
+//  TikTok
+//
+//  Created by mbook on 01/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct TikTokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
